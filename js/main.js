5b1ca3bf694004557c5bc760faf935e6705c8321
seongjongju <sjj990480@gmail.com>
2024-11-11 23:31:36 +0900

    비주얼 반응형 작업해야함

--- js/main.js
+++ js/main.js
@@ -1,5 +1,5 @@
-/* 헤더 lnb 이벤트 */
 $(function() {
+    /* 헤더 lnb 이벤트 */
     const header = $('header');
     const lnb = $('.lnb');
     const depth1 = $('.depth1');
@@ -13,5 +13,15 @@ $(function() {
         lnb.removeClass('down');
         header.removeClass('down');
     });
+    /* 헤더 lnb 이벤트 */
+
+    /* m_gnb */
+    const gnbBtn = $('.gnb_btn');
+    const m_gnb = $('.m_gnb');
+    console.log(m_gnb)
+
+    gnbBtn.on('click', ()=> {
+        m_gnb.toggleClass('on');
+        gnbBtn.toggleClass('on');
+    });
 });
-/* 헤더 lnb 이벤트 */

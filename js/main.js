61e957cf4bda0cfdc69900f562e03053ad6d9d74
seongjongju <sjj990480@gmail.com>
2024-11-14 11:40:48 +0900

    section01 반응형 작업중

--- js/main.js
+++ js/main.js
@@ -1,27 +1,5 @@
-$(function() {
-    /* 헤더 lnb 이벤트 */
-    const header = $('header');
-    const lnb = $('.lnb');
-    const depth1 = $('.depth1');
-
-    depth1.on('mouseover', () => {
-        lnb.addClass('down');
-        header.addClass('down');
-    });
-
-    header.on('mouseleave', () => {
-        lnb.removeClass('down');
-        header.removeClass('down');
-    });
-    /* 헤더 lnb 이벤트 */
-
-    /* m_gnb */
-    const gnbBtn = $('.gnb_btn');
-    const m_gnb = $('.m_gnb');
-    console.log(m_gnb)
-
-    gnbBtn.on('click', ()=> {
-        m_gnb.toggleClass('on');
-        gnbBtn.toggleClass('on');
-    });
-});
+/* include */
+$(document).ready(function () {
+    $("#header").load("include/header.html");
+  });
+/* include */
